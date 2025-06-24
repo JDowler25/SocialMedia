3ee8ab9d0512f3740ba7d12117cf28d26c658700
JDowler25 <134719325+JDowler25@users.noreply.github.com>
2025-06-24 13:33:54 -0700

    Add scaling utility for responsive sizing

---
+++ assets/styles/scaling.js
@@ -0,0 +1,27 @@
+import { Dimensions, PixelRatio } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+// Guideline sizes are based on standard ~5" screen mobile device
+const GUIDELINE_BASE_WIDTH = 375;
+const GUIDELINE_BASE_HEIGHT = 812;
+
+const scaleWidth = SCREEN_WIDTH / GUIDELINE_BASE_WIDTH;
+const scaleHeight = SCREEN_HEIGHT / GUIDELINE_BASE_HEIGHT;
+
+export const horizontalScale = size => size * scaleWidth;
+
+export const verticalScale = size => size * scaleHeight;
+
+export const scaleFont = size => {
+  const newSize = size * scaleWidth;
+  const adjustedSize = DeviceInfo.isTablet() ? newSize * 0.9 : newSize;
+  return Math.round(PixelRatio.roundToNearestPixel(adjustedSize));
+};
+
+export default {
+  horizontalScale,
+  verticalScale,
+  scaleFont,
+};
