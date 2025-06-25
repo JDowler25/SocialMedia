a3430eb9188152ac2b31f3853daff51cd1818ff2
Jaydan Dowler <jaydan.dowler@bigbrandtire.com>
2025-06-25 14:36:37 -0700

    stack navigation good here

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import globalStyle from '../../assets/styles/globalStyle';
 
 const Profile = () => {
   return (
-    <SafeAreaView>
-      <Text>Profile Screen</Text>;
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <Text>Profile Screen</Text>
     </SafeAreaView>
   );
 };
